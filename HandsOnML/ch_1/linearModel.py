7897438dab5974262bb1c667f8cd9a82bd0a8705
Takuma Yagi <takumayagi1205@gmail.com>
2020-04-24 11:38:14 +0900

    updated on 20200424

--- HandsOnML/ch_1/linearModel.py
+++
@@ -1,8 +0,0 @@
-import matplotlib
-import matplotlib.pyplot as plt
-import sklearn
-import pandas as pd
-from pandasql import sqldf as sql
-import numpy as np
-
-covid19_tokyo = pd.read_csv('Desktop/130001_tokyo_covid19_patients.csv')
