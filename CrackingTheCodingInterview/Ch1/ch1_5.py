ba9b959ac1e37683bd827b684cd6d3ccd44019fa
Takuma Yagi <takumayagi1205@gmail.com>
2020-04-25 11:32:02 +0900

    update 20200425

--- CrackingTheCodingInterview/Ch1/ch1_5.py
+++ CrackingTheCodingInterview/Ch1/ch1_5.py
@@ -1,5 +1,14 @@
 import unittest
 
+def check(char1, char2):
+    if len(char1) == len(char2):
+        check_change(char1, char2)
+    elif len(char1) > len(char2):
+        check_insert(char1, char2)
+    else:
+        check_insert(char2, char1)
+
+
 def check_change(char1, char2):
     counter = 0
     num = len(char1)
@@ -10,14 +19,11 @@ def check_change(char1, char2):
         return False
     return True
 
+
 def check_insert(char1, char2):
     index1 = 0
     index2 = 0
-    if len(char1) < len(char2):
-        tmp = char1
-        char1 = char2
-        char2 = tmp
-    elif len(char1) - len(char2) == 2:
+    if len(char1) - len(char2) == 2:
         return False
     else:
         while index1 < len(char1) && index2 < len(char2):
@@ -30,5 +36,23 @@ def check_insert(char1, char2):
             index2 += 1
         return True
 
+
+class Test(unittest.TestCase):
+
+    def test_check(self):
+        dataT = ('abcde', 'adbec')
+        dataF1 = ('abccde', 'abcde')
+        dataF2 = ('akasaka', 'sasazuka')
+
+        actual1 = check(dataT[0], dataT[1])
+        self.assertTrue(actual)
+
+        actual2 = check(dataF1[0], dataF1[1])
+        self.assertFalse(actual)
+
+        actual3 = check(dataF2[0], dataF2[1])
+        self.assertFalse(actual)
+
+        
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
